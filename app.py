44c6ccac7a268c9a1578fa90571f8787b3851985
Ling-Cheng-Yan <darkness851106@gmail.com>
2021-06-30 06:02:23 +0800

    update -12

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=sticker_message))
+        sticker_message)
 
 
 if __name__ == "__main__":
